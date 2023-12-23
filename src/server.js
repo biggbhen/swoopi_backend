ddb17ff2911d5c4cbd8c178d2bb82edb0a7d9945
Ugochukwu Benjamin <benstar531@gmail.com>
2023-12-23 23:59:19 +0100

    update

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 const corsOptions = {
-	origin: 'http://localhost:3000', // replace with your frontend URL
+	origin: '*', // replace with your frontend URL
 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 	credentials: true, // Allow credentials (e.g., tokens)
